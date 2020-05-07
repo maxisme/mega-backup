749c7c4c28958c77995e687576c3216b65927ef8
Maximilian Mitchell <max@maxis.me>
2020-05-07 19:42:05 +0100

    .

--- backup-servers.py
+++ backup-servers.py
@@ -16,27 +16,27 @@ def backup(rsync_cmd, local_dir, remote_dir):
 
 if __name__ == "__main__":
     with open('servers.json') as json_file:
-        s = json.load(json_file)
-        backup_dir = s['global-backup-dir']
-        global_exclude_dirs = s['global-exclude-dirs']
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            s = json.load(json_file)
+            backup_dir = s['global-backup-dir']
+            global_exclude_dirs = s['global-exclude-dirs']
 
-        for server in s["servers"]:
-            port = s["servers"][server]["ssh-port"]
-            host = s["servers"][server]["host"]
-            local_dir = f"{backup_dir}{server}/"
+            for server in s["servers"]:
+                port = s["servers"][server]["ssh-port"]
+                host = s["servers"][server]["host"]
+                local_dir = f"{backup_dir}{server}/"
 
-            exclude_dirs = global_exclude_dirs.copy()
-            rsync_cmd = BASIC_RSYNC_CMDS.copy()
+                exclude_dirs = global_exclude_dirs.copy()
+                rsync_cmd = BASIC_RSYNC_CMDS.copy()
 
-            # fetch all excluded directories
-            exclude_dirs += s["servers"][server]["exclude-dirs"]
+                # fetch all excluded directories
+                exclude_dirs += s["servers"][server]["exclude-dirs"]
 
-            # add exclude dirs to rsync cmd
-            for exclude_dir in exclude_dirs:
-                rsync_cmd.append(f"--exclude={exclude_dir}")
+                # add exclude dirs to rsync cmd
+                for exclude_dir in exclude_dirs:
+                    rsync_cmd.append(f"--exclude={exclude_dir}")
 
-            # add destination rsync cmd
-            rsync_cmd += ["-e", f"ssh -p {port}", f"{host}:/", local_dir]
+                # add destination rsync cmd
+                rsync_cmd += ["-e", f"ssh -p {port}", f"{host}:/", local_dir]
 
-            x = threading.Thread(target=backup, args=(rsync_cmd, local_dir, server))
-            x.start()
+                executor.submit(backup, rsync_cmd, local_dir, server)
